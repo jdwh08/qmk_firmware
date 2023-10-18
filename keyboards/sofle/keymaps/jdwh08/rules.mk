9f3f57545dabfc0a1df150c3c514758aaa27a004
Jonathan Wang <34083301+jdwh08@users.noreply.github.com>
2023-10-17 20:39:10 -0500

    add luna

--- keyboards/sofle/keymaps/jdwh08/rules.mk
+++ keyboards/sofle/keymaps/jdwh08/rules.mk
@@ -17,3 +17,5 @@ AUTO_SHIFT_NO_AUTO_REPEAT_PER_KEY = yes  # allow keys to be excluded from repeat
 AUTOCORRECT_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 OS_DETECTION_ENABLE = yes
+
+WPM_ENABLE = yes  # Needed for OLED
